757e5fcba64029e48047e174efba88934043898f
Victor Eduardo <victornascimento@mx2.unisc.br>
2024-09-11 11:13:18 -0300

    up

---
+++ src/main/java/com/Unisc/TC/TcApplication.java
@@ -0,0 +1,13 @@
+package com.Unisc.TC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcApplication.class, args);
+	}
+
+}
